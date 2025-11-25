3281d07f82c7ccf0bdf2b6b7609ed146d9de924b
ppedvLukasK <lukask@ppedv.de>
2025-11-25 16:57:07 +0100

    M005

---
+++ M005/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace M005;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+	private int counter = 123;
+
+	public int Counter
+	{
+		get => counter;
+		set
+		{
+			counter = value;
+			Notify(nameof(Counter));
+		}
+	}
+
+	public ObservableCollection<int> Zahlen { get; set; } = [];
+
+	public MainWindow()
+	{
+		//this.DataContext = this;
+
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Counter++;
+		//Notify("Counter"); //GUI benachrichtigen
+
+		Zahlen.Add(Random.Shared.Next());
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	public void Notify(string prop) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+}
