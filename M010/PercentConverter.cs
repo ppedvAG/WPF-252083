b669726ab8965c14676ae5948a308787497fdec5
ppedvLukasK <lukask@ppedv.de>
2025-11-27 10:01:41 +0100

    M010

---
+++ M010/PercentConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M010;
+
+internal class PercentConverter : IValueConverter
+{
+	public double Minimum { get; set; }
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		double d = (double)value;
+		return d >= Minimum;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
