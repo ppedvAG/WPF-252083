75c77073a46b651a114c8cfdfcc71c7bb421c3a8
ppedvLukasK <lukask@ppedv.de>
2025-11-27 11:36:13 +0100

    M012

---
+++ M012/HobbiesUnpackConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M012;
+
+class HobbiesUnpackConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return string.Join(", ", value as IEnumerable<string>);
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
